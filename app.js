787a93ee47f758e93a588d3a15ee4ed249bd465e
Tina Wang <tina@tina.cafe>
2023-07-26 14:58:02 +0200

    ⚡ Some small improvements

--- app.js
+++ app.js
@@ -18,16 +18,15 @@ export default {
           "Access-Control-Max-Age": "86400",
           "Access-Control-Allow-Headers": "Content-Type, authorization",
           "Content-Length": "0",
-        }
+        },
       })
-    }
-    else if (method === "GET") {
+    } else if (method === "GET") {
       if (url.pathname === "/ping") return new Response("🏓")
 
       // 🛂
       let remote_url
       try {
-        remote_url = new URL(url.href.slice(url.href.indexOf(url.host) + url.host.length + 1))
+        remote_url = new URL(url.toString().replace(`${url.origin}/`, ""))
       } catch (e) {
         return new Response("", { status: 400 })
       }
@@ -56,28 +55,25 @@ export default {
     } else if (method === "PATCH") {
       if (headers.get("X-API-KEY") !== SHARED_SECRET) return new Response("", { status: 401 })
 
-      const key = url.href.slice(url.href.indexOf(url.host) + url.host.length + 1)
+      const key = url.toString().replace(`${url.origin}/`, "")
       if (!key.length) return new Response("", { status: 400 })
 
       let reqs = []
       CACHE.forEach((_, _key) => {
         if (_key.startsWith(key))
           reqs.push(
-            new Promise(async (resolve, reject) => {
+            new Promise(async resolve => {
               const remote_resp = await fetch(_key, { headers: { referer: process.env.NODE_NAME } })
-              if (remote_resp.status > 299) {
-                CACHE.delete(_key)
-                reject(_key)
-              }
-
-              CACHE.set(_key, compress(Buffer.from(await remote_resp.text()), { quality: 11 }))
+              if (remote_resp.status > 299) CACHE.delete(_key)
+              else CACHE.set(_key, compress(Buffer.from(await remote_resp.text()), { quality: 11 }))
               resolve()
             })
           )
       })
 
       try {
-        await Promise.all(reqs)
+        // ⚡ Splice requets to not overload the remote server
+        while (reqs.length) await Promise.all(reqs.splice(0, 5))
       } catch (error) {
         return new Response(`cache refetch failed: ${error}`, { status: 500 })
       }
@@ -86,7 +82,7 @@ export default {
     } else if (method === "DELETE") {
       if (headers.get("X-API-KEY") !== SHARED_SECRET) return new Response("", { status: 401 })
 
-      const key = url.href.slice(url.href.indexOf(url.host) + url.host.length + 1)
+      const key = url.toString().replace(`${url.origin}/`, "")
 
       if (key.length) {
         CACHE.forEach((_v, _key) => {
